5b9880123c0f4c99d33299bcc66ff605d3556930
practicapython2019 <52998586+practicapython2019@users.noreply.github.com>
2019-07-26 13:30:22 -0300

    Add files via upload

---
+++ eze.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+temperatura = 15
+distancia = 20
+
+if 10 < temperatura < 30:
+    if distancia <= 15:
+        print('lindo dia')
+    else:
+        print('por cole')
+else:
+    print('cole')
